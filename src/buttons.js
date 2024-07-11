2b801ad3314fb4ab12bc1d444787e6caf4e49ffd
Akio <aakocherga@mail.ru>
2024-07-12 00:20:25 +0700

    Add panel btns style and reaction in

--- src/buttons.js
+++ src/buttons.js
@@ -1,3 +1,28 @@
 function activeButton() {
-  const buttons = document.querySelectorAll(".item")
+  const buttons = document.querySelectorAll(".btn-item")
+  const inbox = document.querySelector("#inbox")
+  const today = document.querySelector("#today")
+  const week = document.querySelector("#week")
+
+  // default active btn
+  inbox.classList.add("active")
+
+  buttons.forEach(button => {
+    button.addEventListener("mousedown", () => {
+        if (inbox.classList.contains("active")) {
+          inbox.classList.remove("active")
+        }
+        else if (today.classList.contains("active")) {
+          today.classList.remove("active")
+        }
+        else if (week.classList.contains("active")) {
+          week.classList.remove("active")
+        }
+        button.classList.add("active")
+    })
+  });
+}
+
+module.exports = {
+  activeButton,
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import "./style.css"
 import "./assets/add.svg"
+import { activeButton } from "./buttons"
 
 const content = document.getElementById("content")
+
+function start() {
+  activeButton()
+}
+
+start()
